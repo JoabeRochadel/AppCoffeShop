b9a32d99070d0fbb5a78780faec9706a923d2911
Joabe Rochadel <joaberochadel@gmail.com>
2023-04-03 07:28:26 -0300

    Edição tela usuario, cadastro e tela de login

--- app/src/main/java/devandroid/joabe/projetofirebase/FormCadastro.java
+++ app/src/main/java/devandroid/joabe/projetofirebase/FormCadastro.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class form_login extends AppCompatActivity {
+public class FormCadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_login);
+        setContentView(R.layout.activity_form_cadastro);
+        getSupportActionBar().hide();
     }
 }
